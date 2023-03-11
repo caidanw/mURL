ada2eca80fa7eb210673d7626c5bcf72acea0c8f
Caidan Williams <caidanstevenwilliams@gmail.com>
2023-03-10 18:05:17 -0800

    Add server.js

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const shortid = require("shortid");
+const validUrl = require("valid-url");
+const fs = require("fs");
+
+const app = express();
+const PORT = 3000;
+
+// Set up middleware
+app.use(bodyParser.json());
+
+// Set up database
+const DB_FILE = "urls.json";
+let urls = {};
+try {
+  urls = JSON.parse(fs.readFileSync(DB_FILE, "utf8"));
+  console.log(`Loaded ${Object.keys(urls).length} URLs from database file`);
+} catch (err) {
+  console.log(`Error reading database file: ${err}`);
+}
+
+// Define routes
+app.post("/api/shorten", (req, res) => {
+  const longUrl = req.body.longUrl;
+  const customUrl = req.body.customUrl;
+
+  // Validate the input URL
+  if (!validUrl.isUri(longUrl)) {
+    console.log(`Invalid URL: ${longUrl}`);
+    return res.status(400).json({ error: "Invalid URL" });
+  }
+
+  // Check if a custom short URL was provided
+  let shortUrl;
+  if (customUrl) {
+    // Make sure the custom URL is not already in use
+    if (urls[customUrl]) {
+      console.log(`Custom URL already in use: ${customUrl}`);
+      return res.status(400).json({ error: "Custom URL already in use" });
+    }
+    // Check if the custom URL starts with "api"
+    if (customUrl.startsWith("api")) {
+      console.log(`Custom URL cannot start with 'api': ${customUrl}`);
+      return res.status(400).json({ error: "Custom URL cannot start with 'api'" });
+    }
+    shortUrl = customUrl;
+  } else {
+    // Generate a short URL using shortid
+    shortUrl = shortid.generate();
+  }
+
+  // Save the mapping from short URL to long URL in the database
+  urls[shortUrl] = longUrl;
+  fs.writeFileSync(DB_FILE, JSON.stringify(urls, null, 2));
+  console.log(`Added URL mapping: ${shortUrl} -> ${longUrl}`);
+
+  // Return the short URL with the current request hostname
+  res.json({ shortUrl: `${req.protocol}://${req.hostname}/${shortUrl}` });
+});
+
+app.get("/:shortUrl", (req, res) => {
+  const shortUrl = req.params.shortUrl;
+
+  // Look up the long URL from the database using the short URL
+  const longUrl = urls[shortUrl];
+
+  // If the short URL is not in the database, return a 404 error
+  if (!longUrl) {
+    console.log(`Short URL not found: ${shortUrl}`);
+    return res.status(404).send("Not Found");
+  }
+
+  // Redirect to the long URL
+  console.log(`Redirecting to long URL: ${longUrl}`);
+  res.redirect(longUrl);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
