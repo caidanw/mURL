33086be7632dcd1d195985bd4789d2078e8ae4a4
Caidan Williams <caidanstevenwilliams@gmail.com>
2023-03-15 14:26:54 -0700

    Move inline script tag to app.js

--- public/app.js
+++ public/app.js
@@ -1,22 +1,34 @@
-function shorten() {
-	// Get the input URL and custom short URL (if any)
-	var longUrl = document.getElementById("urlInput").value;
-	var customUrl = document.getElementById("customInput").value;
+const shortUrlElem = document.getElementById("shortUrl");
+shortUrlElem.innerText = `${location.href}r/...`;
 
-	// Make a POST request to the server to create a short URL
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "/api/shorten", true);
-	xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-			// Update the UI with the shortened URL
-			var shortUrl = JSON.parse(xhr.responseText).shortUrl;
-			document.getElementById("shortUrl").value = shortUrl;
-		}
-	};
-	var payload = {longUrl: longUrl};
-	if (customUrl) {
-		payload.customUrl = customUrl;
-	}
-	xhr.send(JSON.stringify(payload));
+function shorten() {
+  // Get the input URL and custom short URL (if any)
+  var longUrl = document.getElementById("urlInput").value;
+  var customUrl = document.getElementById("customInput").value;
+
+  // Make a POST request to the server to create a short URL
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/api/shorten", true);
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      // Update the UI with the shortened URL
+      var shortUrl = JSON.parse(xhr.responseText).shortUrl;
+      shortUrlElem.innerText = shortUrl;
+    }
+  };
+
+  var payload = { longUrl: longUrl };
+  if (customUrl) {
+    payload.customUrl = customUrl;
+  }
+  xhr.send(JSON.stringify(payload));
+}
+
+function copy() {
+  var text = shortUrlElem.innerText;
+  navigator.clipboard.writeText(text);
+
+  document.getElementById("copy").innerText = "Copied";
+  setTimeout(() => (document.getElementById("copy").innerText = "Copy"), 5000);
 }
